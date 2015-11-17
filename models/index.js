c02f7c3da1d3c260511eb45377bea38244039b55
Nathan Ockerman <ockerman.ns@gmail.com>
2015-11-17 15:37:25 -0800

    connecting to db

--- models/index.js
+++ models/index.js
@@ -1,5 +1,9 @@
 var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/connection_board_app");
 
+// *** REQUIREMENTS *** //
+module.exports.User = require("./user.js");
+
+
 // process.env.MONGOLAB_URI ||
 //                       process.env.MONGOHQ_URL ||
