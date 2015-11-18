729bfeebdc9537b297e6aaf59a16f96b6965d166
Nathan Ockerman <ockerman.ns@gmail.com>
2015-11-18 11:53:49 -0800

    basic project view pages added.

--- models/models.js
+++ models/models.js
@@ -4,7 +4,7 @@ var Schema = mongoose.Schema;
 
 // # PROJECT SCHEMA # //
 var ProjectSchema = new Schema({
-                  // code
+                  // code              // Belongs to user
                   title: String,
                   description: String,
                   dateCreated: {
@@ -17,7 +17,7 @@ var ProjectSchema = new Schema({
 
 // # BOARD SCHEMA # //
 var BoardSchema = new Schema({
-                  // code
+                  // code              // Belongs to project
                   name: String,
                   type: String, // (Ex: Default, Map, Timeline, Portfolio, etc…)
                   // CONNECTION LOGIC //
@@ -26,7 +26,7 @@ var BoardSchema = new Schema({
 
 // # CARD SCHEMA # //
 var CardSchema = new Schema({
-                  // code
+                  // code              // Belongs to board
                   name: String,
                   type: String, // (Ex: Person, Event, Location, etc…)
                   // CONNECTION LOGIC //
@@ -35,7 +35,7 @@ var CardSchema = new Schema({
 
 // # STRING SCHEMA # //
 var StringSchema = new Schema({
-                  // code
+                  // code              // Has many cards (min. 2 [more like join table])
                   name: String,
                   type: String // (Ex: Event type [capture, battle, death, birth, etc...])
                   // CONNECTION LOGIC //
